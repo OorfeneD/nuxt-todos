7e453243ec292f74bd5c9e0f46aa1f8cc7b30adb
OorfeneDeuce <oorfened@gmail.com>
2021-10-12 10:32:51 +0000

    new: TodoList vue component.
    list of todos. Handle todo events and emits they to parent.

---
+++ components/TodoList.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul class="py-1">
+    <todo
+      v-for="todo in todos"
+      :key="todo.id"
+      :todo="todo"
+      @removetodo="$emit('removetodo', $event)"
+      @updatetodo="$emit('updatetodo', $event)"
+    />
+  </ul>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import Todo from './Todo.vue'
+export default {
+  components: { Todo },
+  computed: {
+    ...mapGetters(['todos'])
+  }
+}
+</script>
+
+<style>
+
+</style>
