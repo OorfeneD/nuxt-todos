8ddda910d1504decac5785337f18acf1f9e1491f
OorfeneDeuce <oorfened@gmail.com>
2021-10-12 11:17:36 +0000

    new: header title with github profile link.
    links to *OorfeneD* profile page on **GitHub**

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,16 @@
 <template>
   <div class="w-screen min-h-screen flex flex-col items-center px-10 py-2">
-  <div class=" w-96 rounded-xl border border-gray-100 shadow-sm p-5">
-    <add-todo @error="showError" @addtodo="_addTodo" />
-    <todo-list @removetodo="_removeTodo" @updatetodo="_updateTodo" />
+    <div class="py-3 px-2">
+      <h1 class="w-full text-center text-2xl">
+        ToDo List
+      </h1>
+      <span class="text-gray-400 text-sm">
+        by <a href="https://github.com/OorfeneD" class="text-blue-300 hover:bg-gray-100 p-1 rounded" title="GitHub OorfeneD"><icon github /> OorfeneD</a>
+      </span>
+    </div>
+    <div class=" w-96 rounded-xl border border-gray-100 shadow-sm p-5">
+      <add-todo @error="showError" @addtodo="_addTodo" />
+      <todo-list @removetodo="_removeTodo" @updatetodo="_updateTodo" />
     </div>
   </div>
 </template>
@@ -10,9 +18,10 @@
 <script>
 import { mapActions } from 'vuex'
 import AddTodo from '~/components/AddTodo.vue'
+import Icon from '~/components/Icon.vue'
 import TodoList from '~/components/TodoList.vue'
 export default {
-  components: { AddTodo, TodoList },
+  components: { AddTodo, TodoList, Icon },
   methods: {
     ...mapActions(['addTodo', 'removeTodo', 'updateTodo']),
     showError (event) {
