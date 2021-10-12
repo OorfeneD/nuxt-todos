93aabed750aa9af5878a3c3f5a6eaf5a0a9f5c51
OorfeneDeuce <oorfened@gmail.com>
2021-10-12 11:57:41 +0000

    change: add todo event emitter to form submit.
    Now AddTodo component emits addtodo event on form submit.
    Remove emitter from button.click

--- components/AddTodo.vue
+++ components/AddTodo.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex w-full justify-between align-middle">
+  <form class="flex w-full justify-between align-middle" @submit.prevent="addTodo">
     <input v-model="title" class="focus:shadow-inner transition shadow-xs focus:outline-none border border-gray-200 text-gray-600 px-3 py-2 rounded-md flex-grow mr-2" type="text" placeholder="todo title ...">
-    <button class="hover:bg-indigo-500 text-gray-600 hover:text-white transition px-2 py-2 border border-gray-200 rounded-md hover:shadow-sm" @click="$emit('addtodo', { content: { title }})">
+    <button class="hover:bg-indigo-500 text-gray-600 hover:text-white transition px-2 py-2 border border-gray-200 rounded-md hover:shadow-sm">
       Add
     </button>
-  </div>
+  </form>
 </template>
 
 <script>
@@ -12,6 +12,12 @@ export default {
   data () {
     return {
       title: ''
+    }
+  },
+  methods: {
+    addTodo (event) {
+      this.$emit('addtodo', { content: { title: this.title } })
+      this.title = ''
     }
   }
 }
