c1fa36a759e8c91193122a2667ef488f9dfe90a8
OorfeneDeuce <oorfened@gmail.com>
2021-10-18 13:26:06 +0000

    new: TagList vue component.
    used for display multiple tags as list.
    uses tags array as prop

---
+++ components/tags/TagList.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="flex flex-row gap-1">
+    <tag v-for="tagEntry, index in tags" :key="index" :title="tagEntry.title" :color="tagEntry.color" />
+  </div>
+</template>
+
+<script>
+import Tag from './Tag.vue'
+export default {
+  components: { Tag },
+  props: {
+    tags: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
