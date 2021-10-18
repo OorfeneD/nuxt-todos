9aa780336d0772b8adc1d3a84f92ee5e8866d3f7
OorfeneDeuce <oorfened@gmail.com>
2021-10-18 13:30:58 +0000

    change: style of Todo component.
    this is need for adding taglist component below of todo title

--- components/Todo.vue
+++ components/Todo.vue
@@ -1,14 +1,15 @@
 <template>
-  <li :class="`py-2 px-3 border border-gray-100 text-gray-500 rounded-md my-1 flex  ${todo.status ? 'bg-gray-100' : ''}`">
+  <li :class="`border border-gray-100 text-gray-500 rounded-md my-1 pb-2 ${todo.status ? 'bg-gray-100' : ''}`">
+    <div class="pt-2 px-3 flex">
     <input
       :id="todo.id"
-      class="flex-grow-0"
+        class="flex-grow-0 cursor-pointer"
       :checked="todo.status"
       type="checkbox"
       name="todo"
       @change="$emit('updatetodo', { todo, status: $event.target.checked, originalEvent: $event})"
     >
-    <label class="flex-grow text-center pl-1 pr-3" :for="todo.id">
+      <label class="flex-grow text-center pl-1 pr-3 cursor-pointer" :for="todo.id">
       <strike v-if="todo.status" class="text-gray-500 transition transition-all">
         {{ todo.content.title }}
       </strike>
@@ -16,9 +17,10 @@
         {{ todo.content.title }}
       </span>
     </label>
-    <button class="font-bold text-red-800 flex-grow-0 -my-2 -mx-3 px-3 border-l hover:bg-red-400 hover:text-white transition rounded-r" @click="$emit('removetodo', { todo: todo, originalEvent: $event })">
+      <button class="font-bold text-red-800 flex-grow-0 -my-2 -mx-3 px-3 hover:bg-red-400 hover:text-white transition rounded-r" @click="$emit('removetodo', { todo: todo, originalEvent: $event })">
       <icon close />
     </button>
+    </div>
   </li>
 </template>
 
