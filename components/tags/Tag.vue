18dc80c24202c4eda8e13da98bd2032fb9eee972
OorfeneDeuce <oorfened@gmail.com>
2021-10-18 13:24:51 +0000

    new: Tag vue component.
    used to display single tag.
    uses title and color props

---
+++ components/tags/Tag.vue
@@ -0,0 +1,28 @@
+<template>
+  <span
+    class="p-1 text-xs rounded-lg shadow-sm hover:shadow-md cursor-pointer transition pl-2 border border-l-4"
+    :style="`border-left-color: ${color}`"
+    @click="$emit('click', $event)"
+  >
+    {{ title }}
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: 'empty'
+    },
+    color: {
+      type: String,
+      default: 'default'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
