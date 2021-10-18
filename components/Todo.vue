c657bed2582e65f863cc24b1c1ec8a4f45b36128
OorfeneDeuce <oorfened@gmail.com>
2021-10-18 13:32:24 +0000

    new: tags list in todo.
    display todo tags as list of title elements.

--- components/Todo.vue
+++ components/Todo.vue
@@ -1,31 +1,36 @@
 <template>
   <li :class="`border border-gray-100 text-gray-500 rounded-md my-1 pb-2 ${todo.status ? 'bg-gray-100' : ''}`">
     <div class="pt-2 px-3 flex">
-    <input
-      :id="todo.id"
+      <input
+        :id="todo.id"
         class="flex-grow-0 cursor-pointer"
-      :checked="todo.status"
-      type="checkbox"
-      name="todo"
-      @change="$emit('updatetodo', { todo, status: $event.target.checked, originalEvent: $event})"
-    >
+        :checked="todo.status"
+        type="checkbox"
+        name="todo"
+        @change="$emit('updatetodo', { todo, status: $event.target.checked, originalEvent: $event})"
+      >
       <label class="flex-grow text-center pl-1 pr-3 cursor-pointer" :for="todo.id">
-      <strike v-if="todo.status" class="text-gray-500 transition transition-all">
-        {{ todo.content.title }}
-      </strike>
-      <span v-else>
-        {{ todo.content.title }}
-      </span>
-    </label>
+        <strike v-if="todo.status" class="text-gray-500 transition transition-all">
+          {{ todo.content.title }}
+        </strike>
+        <span v-else>
+          {{ todo.content.title }}
+        </span>
+      </label>
       <button class="font-bold text-red-800 flex-grow-0 -my-2 -mx-3 px-3 hover:bg-red-400 hover:text-white transition rounded-r" @click="$emit('removetodo', { todo: todo, originalEvent: $event })">
-      <icon close />
-    </button>
+        <icon close />
+      </button>
+    </div>
+    <div v-if="!!todo.tags" class="flex flex-row px-1 pt-1">
+      <tag-list class="pl-2" :tags="tags.filter(v => todo.tags.includes(v.id))" />
     </div>
   </li>
 </template>
 
 <script>
+import TagList from './tags/TagList.vue'
 export default {
+  components: { TagList },
   props: {
     todo: {
       type: Object,
@@ -35,6 +40,10 @@ export default {
           title: '<i>dead inside</i>'
         }
       })
+    },
+    tags: {
+      type: Array,
+      default: () => []
     }
   }
 }
