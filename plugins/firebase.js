c4d72ab20019387468929bb602b8697017bf69ae
OorfeneDeuce <oorfened@gmail.com>
2021-11-08 14:06:05 +0000

    change: added listener to auth state changed.
    listener update current app state on auth state changes

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,13 +1,17 @@
 import { initializeApp } from 'firebase/app'
-import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut } from 'firebase/auth'
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, getIdToken, signOut } from 'firebase/auth'
 import { firebaseConfig } from '@/service/options' // Need to be defined. File is untracked now
 
 export default function ({ app, store }, inject) {
   initializeApp(firebaseConfig)
   const firebaseAuth = getAuth()
-  onAuthStateChanged(firebaseAuth, (user) => {
+  onAuthStateChanged(firebaseAuth, async (user) => {
     if (user) {
-      store.dispatch('user/setUser', user)
+      const token = await getIdToken(user)
+      await store.dispatch('user/setUser', user)
+      await store.dispatch('user/setToken', token)
+    } else {
+      store.dispatch('user/resetUser')
     }
   })
   try {
