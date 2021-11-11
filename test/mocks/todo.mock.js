aa2fb400832b525537a9a23dc590b102c112e80d
OorfeneDeuce <oorfened@gmail.com>
2021-11-11 11:59:37 +0000

    feat: mock data files for jest testing.
    added todo and user mock data.

---
+++ test/mocks/todo.mock.js
@@ -0,0 +1,16 @@
+export const titleText = 'todo text'
+export const todo = {
+  id: 'hash',
+  status: false,
+  content: {
+    title: titleText
+  }
+}
+export const badTodo = {
+  id: null,
+  status: null,
+  content: {
+    title: null
+  }
+}
+export const todos = [todo]
---
+++ test/mocks/user.mock.js
@@ -0,0 +1,2 @@
+export const login = 'demo@sqbo.me'
+export const password = '123456'
