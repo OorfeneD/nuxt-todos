f6bf2b1fb206354edf8319e2d977406d67738aa1
OorfeneDeuce <oorfened@gmail.com>
2021-11-11 12:01:45 +0000

    feat(test): `SignInModal` component test spec.
    also snapshot file included

---
+++ test/modal.spec/SignUpModal.spec.js
@@ -0,0 +1,37 @@
+import { shallowMount } from '@vue/test-utils'
+import { login, password } from '../mocks/user.mock'
+import SignUpModal from '@/components/modal/SignUpModal'
+
+describe('SignUpModal', () => {
+  let wrapper = shallowMount(SignUpModal)
+  beforeEach(() => {
+    wrapper = shallowMount(SignUpModal)
+  })
+  test('is a Vue instance', () => {
+    expect(wrapper.vm).toBeTruthy()
+  })
+  test('matchs snapshot', () => {
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+  test('emits event on from submit', () => {
+    wrapper.find('input#fir').setValue(login)
+    wrapper.find('input#sec').setValue(password)
+    wrapper.find('form').trigger('submit')
+    expect(wrapper.emitted().submit).toBeTruthy()
+  })
+  test('emitted data has login and pass', () => {
+    wrapper.find('input#fir').setValue(login)
+    wrapper.find('input#sec').setValue(password)
+    wrapper.find('form').trigger('submit')
+    expect(wrapper.emitted().submit[0]).not.toBeUndefined()
+    expect(wrapper.emitted().submit[0][0]).not.toBeUndefined()
+    expect(wrapper.emitted().submit[0][0].data).not.toBeUndefined()
+    expect(wrapper.emitted().submit[0][0].data).toMatchObject([login, password])
+  })
+  test('didn`t emits on empty inputs', () => {
+    wrapper.find('input#fir').setValue('')
+    wrapper.find('input#sec').setValue('')
+    wrapper.find('form').trigger('submit')
+    expect(wrapper.emitted().submit).not.toBeTruthy()
+  })
+})
