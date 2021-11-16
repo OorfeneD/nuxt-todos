92e85f266a6f2a2e2025abc1f3984e4a175cd900
OorfeneDeuce <oorfened@gmail.com>
2021-11-16 12:10:13 +0000

    fix: added required attr to AddTodo input
    provided base form validation by adding an `required` attribute to input

--- components/AddTodo.vue
+++ components/AddTodo.vue
@@ -1,6 +1,6 @@
 <template>
   <form class="flex w-full justify-between align-middle" @submit.prevent="addTodo">
-    <input v-model="title" class="focus:shadow-inner transition shadow-xs focus:outline-none border border-gray-200 text-gray-600 px-3 py-2 rounded-md flex-grow mr-2" type="text" placeholder="todo title ...">
+    <input v-model="title" class="focus:shadow-inner transition shadow-xs focus:outline-none border border-gray-200 text-gray-600 px-3 py-2 rounded-md flex-grow mr-2" type="text" placeholder="todo title ..." required>
     <button class="hover:bg-indigo-500 text-gray-600 hover:text-white transition px-2 py-2 border border-gray-200 rounded-md hover:shadow-sm">
       Add
     </button>
